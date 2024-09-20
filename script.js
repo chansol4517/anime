317dff22b729a0392c6081802eaf15d4fa3a64d4
chansol4517 <ckssol4517@gmail.com>
2024-09-20 14:30:53 +0900

    parseInt를 통해 문자화되어 있는 숫자를 정수형태의 숫자로 강제변환처리

--- script.js
+++ script.js
@@ -9,10 +9,19 @@
     idx: 현재 반복도는 순서값
   })
 */
-
 const btns = document.querySelectorAll("button");
 
 btns.forEach((btn, idx) => {
-  console.log(btn);
-  console.log(idx);
+  //forEach반복문 안쪽에서 반복도는 각 btn요소에 click이벤트 연결
+  btn.addEventListener("click", (event) => {
+    //각 반복도는 btn요소 클릭시 자동으로 전달되는 event.target
+    //클릭이라는 이벤트가 발생한 바로 그 DOM요소를 지칭(event.target)
+    //console.log(event.target);
+    //이벤트가 발생한 해당 DOM요소에 지정된 data-pos라는 정보값 가져옴
+
+    const targetPos = event.target.getAttribute("data-pos");
+    console.log(targetPos); //'1000' 숫자처럼 보이는 문자값
+    const convertedTargetPos = parseInt(targetPos); //'1000' -> 1000 으로 변환
+    console.log(convertedTargetPos);
+  });
 });
